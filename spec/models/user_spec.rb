68b09fc7fda36bcc71bb660533cb17a14d1b1817
mn25md <mn25md@gmail.com>
2024-09-04 16:23:34 +0900

    ユーザー管理指摘修正3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe User, type: :model do
     end
 
     context '新規登録ができない場合' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
       it 'メールアドレスが空では登録できない' do
         @user.email = ''
         @user.valid?
