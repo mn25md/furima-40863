862bfda0fd56a6bb50bb17de410cfdf45e8954b0
mn25md <mn25md@gmail.com>
2024-09-10 19:02:53 +0900

    article削除

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,5 +13,4 @@ class Genre < ActiveHash::Base
   ]
 
   include ActiveHash::Associations
-  has_many :articles
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items, only: [:index, :new, :create, :show,]
   resources :users, only: [:show, :edit, :update]
-  resources :articles
   root to: 'items#index'
 end
