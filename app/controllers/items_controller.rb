acdfb1a69addeea9e48e20455ff5e12a494f6856
mn25md <mn25md@gmail.com>
2024-09-11 19:24:11 +0900

    商品一覧表示機能修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items = Item.order(created_at: :desc)
+    @items = Item.includes(:shipping_cost).order(created_at: :desc)
   end
 
   def new
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    # @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   private
