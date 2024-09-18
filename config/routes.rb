2147c8888d2c7bf0a86943e912a5b7ad21449b5f
mn25md <mn25md@gmail.com>
2024-09-18 15:37:55 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only: [:index, :new, :create, :show,]
+  resources :items, only: [:index, :new, :create, :show, :edit] do
+    resources :orders, only: [:new, :create]
+  end
   resources :users, only: [:show, :edit, :update]
   root to: 'items#index'
 end
--- db/migrate/20240905031417_create_shipping_costs.rb
+++ db/migrate/20240905031417_create_shipping_costs.rb
@@ -1,6 +1,8 @@
 class CreateShippingCosts < ActiveRecord::Migration[7.0]
   def change
     create_table :shipping_costs do |t|
+      t.string :name, null: false 
+      t.integer :cost
 
       t.timestamps
     end
