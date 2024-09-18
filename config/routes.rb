cfe03958ad9882a79e026b3e02f013716eb4c447
mn25md <mn25md@gmail.com>
2024-09-18 16:23:12 +0900

    商品詳細表示機能2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :items, only: [:index, :new, :create, :show, :edit] do
+  resources :items, only: [:index, :new, :create, :show] do
     resources :orders, only: [:new, :create]
   end
   resources :users, only: [:show, :edit, :update]
