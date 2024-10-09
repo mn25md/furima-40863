60a2bc16f415009bd3906982451f62ccd347ffd6
mn25md <mn25md@gmail.com>
2024-10-09 23:17:55 +0900

    商品購入機能作成修正４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,7 +33,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     if @item.destroy
       redirect_to root_path, notice: '商品が削除されました。'
     else
