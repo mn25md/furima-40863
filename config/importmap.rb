5367d6cd89a3cfc05ef38c7a99375d4c29045ed8
mn25md <mn25md@gmail.com>
2024-10-11 22:06:17 +0900

    payjp.js削除

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "item_price", to: "item_price.js"
-pin "payjp", to: "payjp.js" # 修正: 正しいパスに変更
 pin "card", to: "packs/card.js"  # 正しいパスを指定
