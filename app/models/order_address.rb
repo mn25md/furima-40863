3ddfa21f9d8bdf94a159cc479aa8663541ae4eb2
mn25md <mn25md@gmail.com>
2024-10-13 00:23:19 +0900

    adress修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,12 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
-    validates :addresses
+    validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
     validates :phone_number, length: { in: 10..11, message: 'is too short or too long' }
 
@@ -24,7 +24,7 @@ class OrderAddress
       post_code:,
       prefecture_id:,
       city:,
-      addresses:,
+      address:,
       building:,
       phone_number:,
       order_id: order.id
